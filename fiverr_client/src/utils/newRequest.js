84e3cf624901ec4976350a19a7abdd22dbec9f7d
chauh <chauhankishan1608@gmail.com>
2023-06-30 21:58:37 +0530

    build file

--- fiverr_client/src/utils/newRequest.js
+++ fiverr_client/src/utils/newRequest.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-    baseURL: "http://localhost:5000/api/",
+    // baseURL: "http://localhost:5000/api/",
+    baseURL: "https://fiverr-clone-wdnk.onrender.com/api/",
     withCredentials: true,
 })
 
